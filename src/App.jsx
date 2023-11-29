42c25fc44c1d138b231dbc4ecb51e72a2bb36cf2
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-11-29 19:19:01 +0600

    Added: set up the store

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Provider } from 'react-redux';
 import { Outlet } from 'react-router-dom';
+import store from './app/store';
 
 function App() {
 	return (
-		<div className='w-full min-h-screen bg-slate-900 text-white'>
-			<Outlet />
-		</div>
+		<Provider store={store}>
+			<div className='w-full min-h-screen bg-slate-900 text-white'>
+				<Outlet />
+			</div>
+		</Provider>
 	);
 }
 
